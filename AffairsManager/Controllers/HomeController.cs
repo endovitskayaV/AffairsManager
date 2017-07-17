852fd928ca500b8049149d79400ef2d752d70a2e
Valentina Endovitskaya <valyalark@hotmail.com>
2017-07-17 11:14:44 +0300

    Http GET for Edit if done

--- AffairsManager/Controllers/HomeController.cs
+++ AffairsManager/Controllers/HomeController.cs
@@ -11,9 +11,20 @@ namespace AffairsManager.Controllers
     {
         AffairsContext db = new AffairsContext();
 
+        [HttpGet]
+        public ActionResult AddAffair()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAffair(Affairs affairs)
+        {
+            return View("Index", affairs);
+        }
+
         public ActionResult Index()
         {
-           
             return View(db.Affairs);
         }
 
@@ -23,11 +34,39 @@ namespace AffairsManager.Controllers
             base.Dispose(disposing);
         }
 
-        public ActionResult Edit()
+        [HttpGet]
+        public ActionResult Edit(int? id)
         {
-            
+            //НЕБЕЗОПАСНО!!!!!!!!
+            /*ViewBag.Name = affair.Name;
+            ViewBag.Description = affair.Description;
+            ViewBag.Category = affair.Category;*/
+
+
+            if (id != null)
+            {
+                Affairs affair = db.Affairs.FirstOrDefault(x => x.Id == id);
+                if (affair != null)
+                    return View(affair);
+            }
+            return HttpNotFound();
+        }
+
+        [HttpPost]
+
+        public ActionResult Edit(Affairs affair)
+        {
+            db.Affairs.Add(affair);
+            db.SaveChanges();
+            return View("Index");
+        }
+
+        public ActionResult ChooseRandomAffair()
+        {
             return View();
         }
+
+
 
     }
 }
--- AffairsManager/Models/Affairs.cs
+++ AffairsManager/Models/Affairs.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace AffairsManager.Models
 {
+    
     public class Affairs
     {
         public int Id { get; set; }
