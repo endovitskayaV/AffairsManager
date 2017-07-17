19b33b3bbac9a87b05c18c1d4d3d18866f984462
Valentina Endovitskaya <valyalark@hotmail.com>
2017-07-17 12:58:37 +0300

    Choosing random affair is done

--- AffairsManager/Controllers/HomeController.cs
+++ AffairsManager/Controllers/HomeController.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AffairsManager.Models;
-
+using System.Data.Entity.Migrations;
 namespace AffairsManager.Controllers
 {
     public class HomeController : Controller
     {
         AffairsContext db = new AffairsContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Affairs);
+        }
 
         [HttpGet]
         public ActionResult AddAffair()
@@ -18,31 +23,16 @@ namespace AffairsManager.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddAffair(Affairs affairs)
+        public ActionResult AddAffair(Affairs affair)
         {
-            return View("Index", affairs);
-        }
-
-        public ActionResult Index()
-        {
-            return View(db.Affairs);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
+            db.Affairs.Add(affair);
+            db.SaveChanges();
+            return View("Index", db.Affairs);
         }
 
         [HttpGet]
         public ActionResult Edit(int? id)
         {
-            //НЕБЕЗОПАСНО!!!!!!!!
-            /*ViewBag.Name = affair.Name;
-            ViewBag.Description = affair.Description;
-            ViewBag.Category = affair.Category;*/
-
-
             if (id != null)
             {
                 Affairs affair = db.Affairs.FirstOrDefault(x => x.Id == id);
@@ -53,19 +43,49 @@ namespace AffairsManager.Controllers
         }
 
         [HttpPost]
-
         public ActionResult Edit(Affairs affair)
         {
-            db.Affairs.Add(affair);
+            db.Affairs.AddOrUpdate(affair);
             db.SaveChanges();
-            return View("Index");
+            return View("Index", db.Affairs);
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int? id)
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                Affairs affair = db.Affairs.FirstOrDefault();
+                if (affair != null)
+                {
+                    db.Affairs.Remove(affair);
+                    db.SaveChanges();
+                    return View("Index", db.Affairs);
+                }
+            }
+            return HttpNotFound();
         }
 
         public ActionResult ChooseRandomAffair()
         {
-            return View();
+            Random rnd = new Random();
+            List<Affairs> affairsList = db.Affairs.ToList();
+            int index = rnd.Next(0, affairsList.Count);
+            return View("Random", affairsList[index]);
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
 
 
     }
