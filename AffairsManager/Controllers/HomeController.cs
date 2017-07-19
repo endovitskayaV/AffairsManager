0b14657363c231eed886ae574f1dea9e36ecb5eb
Valentina Endovitskaya <valyalark@hotmail.com>
2017-07-19 18:04:33 +0300

    sorting is done but it works only with button

--- AffairsManager/Controllers/HomeController.cs
+++ AffairsManager/Controllers/HomeController.cs
@@ -5,15 +5,87 @@ using System.Web;
 using System.Web.Mvc;
 using AffairsManager.Models;
 using System.Data.Entity.Migrations;
+using System.ComponentModel.DataAnnotations;
+
 namespace AffairsManager.Controllers
 {
     public class HomeController : Controller
     {
-        AffairsContext db = new AffairsContext();
-
-        public ActionResult Index()
+        public SortCriteria sortCriteria;
+        public enum SortCriteria
         {
-            return View(db.Affairs);
+            //[Display (Name= "Имя А-Я")]      
+            NameAsc, //возрастанию
+            NameDesc, //убыванию
+            CategoryAsc,
+            CategoryDesc,
+            DateAsc, // сначала добавленные давно
+            DateDesc //сначала последние добавленные
+        }
+        private AffairsContext db = new AffairsContext();
+
+        public ActionResult Index(SortCriteria sortCriteria = SortCriteria.DateAsc)
+        {
+            List<Affairs> affairsList = db.Affairs.ToList();
+
+            IOrderedEnumerable<Affairs> affairs = affairsList.OrderBy(x => x.Id);
+            switch (sortCriteria)
+            {
+                case SortCriteria.NameAsc:
+                    affairs = affairsList.OrderBy(x => x.Name);
+                    break;
+                case SortCriteria.NameDesc:
+                    affairs = affairsList.OrderByDescending(x => x.Name);
+                    break;
+                case SortCriteria.CategoryAsc:
+                    affairs = affairsList.OrderBy(x => x.Category);
+                    break;
+                case SortCriteria.CategoryDesc:
+                    affairs = affairsList.OrderByDescending(x => x.Category);
+                    break;
+                case SortCriteria.DateDesc:
+                    //affairsList.Reverse();
+                    affairs = affairsList.OrderByDescending(x => x.Id); //НЕКРАСИВО!!!
+                    break;
+            }
+
+            return View("Index", affairs.ToList());
+
+            /* ViewBag.Criteria=new SelectList(
+                new List<SortCriteria>
+                {SortCriteria.CategoryAsc,SortCriteria.CategoryDesc,
+                 SortCriteria.DateAsc,SortCriteria.DateDesc,
+                 SortCriteria.NameAsc, SortCriteria.NameDesc});*/
+            // return View(db.Affairs);
+        }
+
+        [HttpPost]
+        public ActionResult Sort(SortCriteria sortCriteria=SortCriteria.DateAsc)
+        {
+            List<Affairs> affairsList = db.Affairs.ToList();
+
+            IOrderedEnumerable<Affairs> affairs= affairsList.OrderBy(x => x.Id);
+            switch (sortCriteria)
+            {
+                case SortCriteria.NameAsc:
+                   affairs =affairsList.OrderBy(x => x.Name);
+                        break;
+                case SortCriteria.NameDesc:
+                    affairs=affairsList.OrderByDescending(x => x.Name);
+                    break;
+                case SortCriteria.CategoryAsc:
+                    affairs=affairsList.OrderBy(x => x.Category);
+                    break;
+                case SortCriteria.CategoryDesc:
+                    affairs=affairsList.OrderByDescending(x => x.Category);
+                    break;
+                case SortCriteria.DateDesc:
+                    //affairsList.Reverse();
+                    affairs=affairsList.OrderByDescending(x => x.Id); //НЕКРАСИВО!!!
+                    break;
+            }
+
+            return View("Index",affairs.ToList());
         }
 
         [HttpGet]
